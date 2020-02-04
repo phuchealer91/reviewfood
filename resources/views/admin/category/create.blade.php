d7b225f4dfcc175afdc935998f45bd77f12449ea
phuchealer91 <pepegvcn2@gmail.com>
2020-02-04 08:14:11 +0700

    Done view create category

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+@section('content')
+    <div class="page-header">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('api-admin.index')}}">Trang chủ</a></li>
+                <li class="breadcrumb-item"><a href="{{route('admin.index.category')}}">Danh mục</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Thêm mới</li>
+            </ol>
+        </nav>
+        </ol>
+    </div>
+    {{--    <div class="container-fluid">--}}
+    <div class="row">
+        <div class="col-md-12">
+            @include('admin.category.form')
+        </div>
+    </div>
+    {{--    </div>--}}
+
+@endsection
